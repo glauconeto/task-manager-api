3ee3a3930c0733475f2419396cad7d2d5ed81f91
glauconeto <glauconeto@tutanota.com>
2026-07-14 11:49:21 -0300

    Fix: troca de nomenclatura

--- src/main/java/io/github/glauconeto/taskmanager/ApiRestApplication.java
+++ src/main/java/io/github/glauconeto/taskmanager/ApiRestApplication.java
@@ -1,4 +1,4 @@
-package io.github.glauconeto.task;
+package io.github.glauconeto.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
