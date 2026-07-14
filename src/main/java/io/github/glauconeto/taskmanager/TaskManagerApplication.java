2772682a21cca080c2c575c75b89e25aeff04c65
glauconeto <glauconeto@tutanota.com>
2026-07-14 12:15:10 -0300

    Feat: criação de diretórios, além de configuração yml

--- src/main/java/io/github/glauconeto/taskmanager/TaskManagerApplication.java
+++ src/main/java/io/github/glauconeto/taskmanager/TaskManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestApplication {
+public class TaskManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApplication.class, args);
+    }
 
 }
