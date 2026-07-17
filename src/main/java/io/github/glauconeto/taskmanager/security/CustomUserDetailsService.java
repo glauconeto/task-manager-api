a152a0e515b930a6274e5b9bf25359f15d7b2dc3
glauconeto <glauconeto@tutanota.com>
2026-07-17 10:48:22 -0300

    Feat: pacote security

---
+++ src/main/java/io/github/glauconeto/taskmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package io.github.glauconeto.taskmanager.security;
+
+import io.github.glauconeto.taskmanager.entity.User;
+import io.github.glauconeto.taskmanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(java.util.Collections.emptyList())
+                .build();
+    }
+
+}
